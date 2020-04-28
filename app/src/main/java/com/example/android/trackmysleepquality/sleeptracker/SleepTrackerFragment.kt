7c1580f0ed9c9ef7db1274ef5429cbe04641e4cc
Kirill <t0nysh4rk@gmail.com>
2020-04-28 03:29:05 +0300

    Switched from linear to grid rw layout manager.

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -25,6 +25,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.database.SleepDatabase
 import com.example.android.trackmysleepquality.databinding.FragmentSleepTrackerBinding
@@ -62,6 +63,9 @@ class SleepTrackerFragment : Fragment() {
       val adapter = SleepNightAdapter()
 
         binding.sleepTrackerViewModel = sleepTrackerViewModel
+
+        val manager = GridLayoutManager(activity, 3)
+        binding.sleepList.layoutManager = manager
 
         binding.sleepList.adapter = adapter
         binding.setLifecycleOwner(this)
